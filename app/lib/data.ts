14d23048b03a09cc5366694c683ab9d4aed55a0b
hanyongyan <hanyongyan8@gmail.com>
2026-06-30 23:10:08 +0800

    remove Promise delay

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,13 +16,7 @@ export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
-
-    console.log("Fetching revenue data...");
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
     const revenue = await sql<Revenue[]>`SELECT * FROM revenue`;
-
-    console.log("Data fetch completed after 3 seconds.");
-
     return revenue;
   } catch (error) {
     console.error("Database Error:", error);
